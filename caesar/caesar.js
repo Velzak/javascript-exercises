884be81441815fead5e40bedea665423acf7c9e8
Robby Noyes <robertjnoyes1@gmail.com>
2021-02-11 09:58:39 -0800

    completed fibonacci and caesar(needed help with caesar)

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,25 @@
-const caesar = function() {
+const caesar = function (str, shift) {
+  return str
+    .split("")
+    .map((char) => shiftChar(char, shift))
+    .join("");
+};
 
+//deciphering if char is upper or lowercase in unicode
+const codeSet = code => (code < 97 ? 65 : 97)
+
+//allowing for negative numbers to work using modulo
+const mod = (n,m) => (n % m + m) % m
+
+const shiftChar = (char, shift) => {
+    const code = char.charCodeAt()
+
+    if((code >= 65 && code <= 90) || (code >= 97 && code <= 122)){
+        return String.fromCharCode(
+            mod(code + shift - codeSet(code), 26) + codeSet(code)
+        )
+    }
+    return char
 }
 
-module.exports = caesar
+module.exports = caesar;
--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (Number(num) < 2){
+        if( num < 0){
+            return 'OOPS'
+        } else {
+            return Number(num)
+        }
+    }
+    return fibonacci(num - 1) + fibonacci(num - 2)
 }
 
 module.exports = fibonacci
